573208cb478a9f4a43f261b22ae468475095cba4
danielvivacqua <drvivacqua@hotmail.com>
2018-01-29 11:39:59 -0500

    Added Cat class and attributes

--- IntroToOOPSpring18/Cat.cs
+++ IntroToOOPSpring18/Cat.cs
@@ -8,8 +8,45 @@ namespace IntroToOOPSpring18
 {
     class Cat
     {
-        //I need STATES
+       
+        //fields
         private string name;
-        //I need BEHAVIORS
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
+
+       //Properties
+       public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        //Constructors
+        public Cat()
+        {
+            //default constructor
+            //takes no parameters (nothing in the parentheses)
+        }
+
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+
+        //methods
+        public void Eat()
+        {
+            if(isHungry)
+            {
+                isHungry = false;
+            }
+
+            Console.WriteLine("Is the cat hungry? " + isHungry);
+        }
+
+
     }
 }
